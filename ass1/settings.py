6de114c5d62e23943482bf60e231e19664c6d073
Henry Opie <Henry.r.opie@gmail.com>
2020-03-28 20:27:23 +1100

    Shitty

--- ass1/settings.py
+++ ass1/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,4 +133,4 @@ STATICFILES_DIRS = (
     # Don't forget to use absolute paths, not relative paths.
     '/static/',
 )
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
