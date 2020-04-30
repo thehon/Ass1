9f9e33656913d158291e2409ade80f9b6671dca9
Henry Opie <Henry.r.opie@gmail.com>
2020-04-30 13:20:21 +1000

    static

--- ass1/settings.py
+++ ass1/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '/static/',
+    #'/static/',
 )
 #SECURE_SSL_REDIRECT = True
 
